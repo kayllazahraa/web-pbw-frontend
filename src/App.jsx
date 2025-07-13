2206fe95ea66ba84df1ffaeea7cfe2a4f17a1f49
kayllazahraa <168892439+kayllazahraa@users.noreply.github.com>
2025-07-13 14:20:21 +0700

    edit fungsional

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,13 @@
 // src/App.jsx
 import React from "react";
-import {
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom";
+import { Routes, Route, Navigate } from "react-router-dom";
 
 import ProtectedRoute from "./components/ProtectedRoute";
 import Navbar from "./components/Navbar";
 import PublicationListPage from "./components/PublicationListPage";
 import AddPublicationPage from "./components/AddPublicationPage";
 import EditPublicationPage from "./components/EditPublicationPage";
-import Footer from "./components/Footer";
-import LoginPage from "./components/LoginPage";
-
-
-export default function App() {// src/App.jsx
-import React from "react";
-import {
-    Routes,
-    Route,
-    Navigate,
-} from "react-router-dom";
-
-import ProtectedRoute from "./components/ProtectedRoute";
-import Navbar from "./components/Navbar";
-import PublicationListPage from "./components/PublicationListPage";
-import AddPublicationPage from "./components/AddPublicationPage";
-import EditPublicationPage from "./components/EditPublicationPage";
-import PublicationDetailPage from "./components/PublicationDetailPage"; // <-- Impor komponen baru
+import PublicationDetailPage from "./components/PublicationDetailPage"; // Pastikan komponen ini ada
 import Footer from "./components/Footer";
 import LoginPage from "./components/LoginPage";
 
@@ -38,36 +17,16 @@ export default function App() {
             <Navbar />
             <main className="container mx-auto px-4 py-8">
                 <Routes>
-                    {/* Public Route */}
+                    {/* Rute Publik */}
                     <Route path="/login" element={<LoginPage />} />
 
-                    {/* Protected Routes */}
+                    {/* Rute Terlindungi */}
                     <Route path="/publications" element={<ProtectedRoute><PublicationListPage /></ProtectedRoute>} />
                     <Route path="/publications/add" element={<ProtectedRoute><AddPublicationPage /></ProtectedRoute>} />
                     <Route path="/publications/edit/:id" element={<ProtectedRoute><EditPublicationPage /></ProtectedRoute>} />
-                    <Route path="/publications/:id" element={<ProtectedRoute><PublicationDetailPage /></ProtectedRoute>} /> {/* <-- Tambahkan rute ini */}
+                    <Route path="/publications/:id" element={<ProtectedRoute><PublicationDetailPage /></ProtectedRoute>} />
 
-                    {/* Redirect Routes */}
-                    <Route path="/" element={<Navigate to="/publications" replace />} />
-                    <Route path="*" element={<Navigate to="/publications" replace />} />
-                </Routes>
-            </main>
-            <Footer />
-        </div>
-    );
-}
-    return (
-        <div className="bg-brand-light min-h-screen font-sans text-text-primary">
-            <Navbar />
-            <main className="container mx-auto px-4 py-8">
-                <Routes>
-                    {/* Public Route */}
-                    <Route path="/login" element={<LoginPage />} />
-                    {/* Protected Routes */}
-                    <Route path="/publications" element={<ProtectedRoute><PublicationListPage /></ProtectedRoute>} />
-                    <Route path="/publications/add" element={<ProtectedRoute><AddPublicationPage /></ProtectedRoute>} />
-                    <Route path="/publications/edit/:id" element={<ProtectedRoute><EditPublicationPage /></ProtectedRoute>} />
-                    {/* Redirect Routes */}
+                    {/* Rute Pengalihan */}
                     <Route path="/" element={<Navigate to="/publications" replace />} />
                     <Route path="*" element={<Navigate to="/publications" replace />} />
                 </Routes>
